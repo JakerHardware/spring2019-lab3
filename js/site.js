34f12b0782ecfa728323daac6497dbf18f11f681
JakerHuber <30348496+JakerHuber@users.noreply.github.com>
2019-02-21 21:32:37 -0600

    Update site.js
    Site now has working JS

--- js/site.js
+++ js/site.js
@@ -6,49 +6,12 @@ jQuery(function($) {
 
 // functions
 function checksubmit(){
-    alert('hi');
+    alert('Submission Received: Congratulations, you are now signed up for our newsletter!');
     
     // disable submit button;
     var submitbutton = document.getElementById("submit");
     submitbutton.style.opacity = 0.2;
     submitbutton.style.backgroundColor = lightgrey;
-    
-    return false;
 };
-
-/***
-function myEventFunction(){
-    // local variables
-    var agecheck = document.getElementById("age-verification");
-    var submitbutton = document.getElementById("submit");
-    var emailbox = document.getElementById("email");
-    var text = "Congratulations, You are now signed up for our Newsletter.";
-
-    // checks if true
-    if(this.checksValidation()){
-        alert('Valid');
-    }
-
-    if((agecheck.checked == true) && (emailbox.checkValidation() == true)){
-
-        // prompt user
-        alert('Submission Confirmed: '+ text);
-
-        // disable submit button;
-        submitbutton.style.opacity = 0.2;
-        submitbutton.style.backgroundColor = lightgrey;
-
-    } else{
-        alert('Not Submitted');
-        submitbutton.classList.add(".notsubmitted");
-        submitbutton.classList.remove(".submitted");
-    }
-}
-*/
-
-// main
-
-var f1 = document.getElementById('submit'); //create variable
-// f1.addEventListener('click', myEventFunction); //run method
 
 document.getElementById("Test1").addEventListener('submit', checksubmit);
